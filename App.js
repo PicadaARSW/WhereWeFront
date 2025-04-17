489a932e29e1d7d80d4758719265cf9db4e509ae
LaaSofiaa <laura.gil-c@mail.escuelaing.edu.co>
2025-04-17 17:22:56 -0500

    fix one issue

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ import GroupDetailScreen from "./screens/GroupDetailScreen";
 import GroupMapScreen from "./screens/GroupMapScreen";
 import ProfilePictureSettings from "./screens/ProfilePictureSettings";
 import EditProfileScreen from "./screens/EditProfileScreen";
+import 'react-native-reanimated';
 
 const Stack = createStackNavigator();
 
